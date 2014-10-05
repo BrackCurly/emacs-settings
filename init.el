39b44c7f637194d687cecabf36d0d7b05b550b15
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2014-10-05 20:36:24 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,22 @@
+;; init package repositories
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+(package-initialize)
+
+;; install packages
+(defvar my-packages '(projectile
+		      clojure-mode
+		      cider
+		      color-theme-solarized))
+
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
+;; disable menu bar and tool bar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+;; load color scheme
+(load-theme 'solarized-dark t)
