1742a8bc07bc4f91fe60d96a6301bb62b76d89df
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-10-28 15:40:48 +0100

    Set indentation to 2 spaces for webmode

--- init.el
+++ init.el
@@ -50,9 +50,14 @@
 (global-set-key [f12] 'indent-buffer)
 (add-hook 'before-save-hook 'indent-buffer)
 
-;; indentation levels
-(setq js-indent-level 2)
-
 ;; web mode
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
